8db24f3a1850b762b380f71b609170b54583bd72
fikri <me@fikri.tech>
2020-10-20 11:46:00 +0700

    nested stack navigator

--- src/screens/LatihanNavigator/StackNavigator.js
+++ src/screens/LatihanNavigator/StackNavigator.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Text, View } from 'react-native'
 import { createStackNavigator } from '@react-navigation/stack'
+import { TouchableOpacity } from 'react-native-gesture-handler'
 
 const Register =(props) => {
     return(
@@ -10,6 +11,9 @@ const Register =(props) => {
             </Text>
             <Text style={{color:"blue",fontStyle :"italic"}} onPress={() =>props.navigation.navigate("login") }>
                 Go To Login
+            </Text>
+            <Text style={{color:"blue",fontStyle :"italic"}} onPress={() =>props.navigation.navigate("homepage") }>
+                Go To Homepage
             </Text>
         </View>
     )
@@ -26,6 +30,95 @@ const Login =() => {
     )
 }
 
+const Account = () => {
+    return(
+        <View style={{justifyContent : "center",alignItems : "center",height : "100%"}}>
+            <Text>
+                Account Page
+            </Text>
+        </View>
+    )
+}
+
+const Chat = () => {
+    return(
+        <View style={{justifyContent : "center",alignItems : "center",height : "100%"}}>
+            <Text>
+                Chat Page
+            </Text>
+        </View>
+    )
+}
+
+
+const Dashboard = ({navigation}) => {
+    return(
+        <View style={{flexDirection : "row"}}>
+           <View style={{width : "33%",borderColor : "black",borderWidth : 2,height : 40,justifyContent : "center",alignItems : "center"}}>
+
+                <Text onPress={() => navigation.navigate('account')}>Account</Text>
+           </View>
+           <View style={{width : "33%",borderColor : "black",borderWidth : 2,height : 40,justifyContent : "center",alignItems : "center"}}>
+                <Text  onPress={() => navigation.navigate('chat')}>Chat</Text>
+           </View>
+           <View style={{width : "33%",borderColor : "black",borderWidth : 2,height : 40,justifyContent : "center",alignItems : "center"}}>
+                <Text  onPress={() => navigation.navigate('products')}>Products</Text>
+           </View>
+           
+        </View>
+    )
+}
+
+
+
+
+const ProductList = ({navigation}) => {
+    return(
+        <View style={{justifyContent : "center",alignItems : "center",height : "100%"}}>
+            <Text>
+                Products Page
+            </Text>
+            <Text onPress={() => navigation.navigate('detail')}>
+                Products Detail
+            </Text>
+        </View>
+    )
+}
+
+const ProductDetail = () => {
+    return(
+        <View style={{justifyContent : "center",alignItems : "center",height : "100%"}}>
+            <Text>
+                Products Detail
+            </Text>
+        </View>
+    )
+}
+
+
+
+
+const StackProduct = createStackNavigator()
+const ProductStack = () => {
+    return(
+        <StackProduct.Navigator>
+            <StackProduct.Screen name='products' component={ProductList} />
+            <StackProduct.Screen name='detail' component={ProductDetail} />
+        </StackProduct.Navigator>
+    )
+}
+
+const HomeStack = createStackNavigator()
+const HomePage =() => {
+    return(
+       <HomeStack.Navigator>
+           <HomeStack.Screen name='dashboard' component={Dashboard} />
+           <HomeStack.Screen name='account' component={Account} />
+           <HomeStack.Screen name='chat' component={Chat} />
+           <HomeStack.Screen name='products' component={ProductStack} />
+       </HomeStack.Navigator>
+    )
+}
 
 const Stack = createStackNavigator()
 const MyStack = () => {
@@ -34,6 +127,7 @@ const MyStack = () => {
           
            <Stack.Screen name='register' component={Register} />
            <Stack.Screen name='login' component={Login} />
+           <Stack.Screen name='homepage' component={HomePage} />
        </Stack.Navigator>
    )
 }
@@ -43,3 +137,14 @@ export default MyStack
 
 
 
+
+
+
+// login
+// register
+// homepage
+    // account
+    // chat
+    // productList
+        // product Detail
+    // cart
